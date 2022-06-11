e39becd68f563f178865caaad961cabe4f02d5aa
Swetha Neeli <swetha.vandanapu@gmail.com>
2022-06-11 04:56:26 +0000

    completed

---
+++ index.js
@@ -0,0 +1,12 @@
+const element = (
+  //   Write your code here.
+  <div className="bg-superLeague" text="center">
+    <h1 className="heading">Super Over League</h1>;
+    <div>
+      <img src="https://assets.ccbp.in/frontend/react-js/rcb-img.png" className="image" />;
+      <img src="https://assets.ccbp.in/frontend/react-js/csk-img.png" className="image" />;
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
